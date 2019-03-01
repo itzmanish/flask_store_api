5829baed52363c951babf3e2f4f5183fa0428e1c
Manish <itzmanish108@gmail.com>
2019-03-01 14:01:26 +0530

    add: some callback added for jwt

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 # importing neccessary library
 import os
-from flask import Flask
+from flask import Flask, jsonify
 from flask_restful import Resource, Api
 from flask_jwt_extended import JWTManager
 from resources.users import UserRegister, User, UserLogin, TokenRefresh
@@ -32,6 +32,46 @@ def add_claims_to_jwt(identity):
     if user.is_admin:
         return False  # little bit workarround for further simplicity in resources
     return True
+
+
+@jwt.expired_token_loader
+def expired_token_callback():
+    return jsonify({
+        'description': 'The token has expired.',
+        'error': 'token_expired'
+    }), 401
+
+
+@jwt.invalid_token_loader
+def invalid_token_callback(error):
+    return jsonify({
+        'description': 'Signature verification failed.',
+        'error': 'invalid_token'
+    }), 401
+
+
+@jwt.unauthorized_loader
+def missing_token_callback(error):
+    return jsonify({
+        'description': 'Request does not contain an access token.',
+        'error': 'unauthorised_token'
+    }), 401
+
+
+@jwt.needs_fresh_token_loader
+def token_not_fresh_callback():
+    return jsonify({
+        'description': 'The token is not fresh.',
+        'error': 'fresh_token_required'
+    }), 401
+
+
+@jwt.revoked_token_loader
+def revoked_token_callback():
+    return jsonify({
+        'description': 'The token has been revoked.',
+        'error': 'token_revoked'
+    }), 401
 
 
 # route resource and register custom resource to Resource
